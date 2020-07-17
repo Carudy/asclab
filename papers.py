399ead24076a6543a32391f333cff35854a3f5e8
ZhaoZhixin <zhaozx@nuaa.edu.cn>
2020-07-17 12:13:16 +0800

    update members

--- papers.py
+++ papers.py
@@ -1,14 +1,15 @@
+from collections import defaultdict as dd
 out = ''
 
-from collections import defaultdict as dd
 
 a = dd(list)
 
 for paper in open('papers.txt').read().split('#'):
     content = list(filter(len, paper.split('\n')))
-    if len(content)==5: 
+    if len(content) == 5:
         year, cont = int(content[0]), content[1:]
-        html = '<li>{}, <i>{},</i> <a href="{}">{}</a></li>'.format(cont[0], cont[1], cont[3], cont[2])
+        html = '<li>{}, <i>{},</i> <a href="{}">{}</a></li>'.format(
+            cont[0], cont[1], cont[3], cont[2])
         a[year].append(html)
 
 for year in sorted(list(a.keys()))[::-1]:
@@ -16,6 +17,7 @@ for year in sorted(list(a.keys()))[::-1]:
 
 fp = open('publication.html', 'w')
 
-fp.write(open('matter/head.html').read() + '\n<div class="container" id="papers">\n')
+fp.write(open('matter/publication-head.html').read() +
+         '\n<div class="container" id="papers">\n')
 fp.write(out)
 fp.write('</div>\n' + open('matter/foot.html').read())
